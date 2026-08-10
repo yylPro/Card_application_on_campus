b97d33a7623a7e6aa5f4dd85ea6658e9cffef361
yulinpro <2023211606@bupt.cn>
2026-08-10 15:44:45 +0800

    完善数据库部署说明并配置本地SQL Server

--- database/sqlserver.sql
+++ database/sqlserver.sql
@@ -1,6 +1,27 @@
-IF DB_ID(N'CampusService') IS NULL CREATE DATABASE CampusService;
+/*
+  Local SQL Server example. Run the CREATE DATABASE block from master.
+  Change the D: paths if the SQL Server service account cannot write there.
+*/
+IF DB_ID(N'CampusService') IS NULL
+BEGIN
+  CREATE DATABASE [CampusService]
+  ON PRIMARY
+  (
+    NAME = N'CampusService',
+    FILENAME = N'D:\CampusServiceData\CampusService.mdf',
+    SIZE = 64MB,
+    FILEGROWTH = 32MB
+  )
+  LOG ON
+  (
+    NAME = N'CampusService_log',
+    FILENAME = N'D:\CampusServiceData\CampusService_log.ldf',
+    SIZE = 32MB,
+    FILEGROWTH = 16MB
+  );
+END;
 GO
-USE CampusService;
+USE [CampusService];
 GO
 IF OBJECT_ID(N'dbo.campus_snapshot', N'U') IS NULL
 CREATE TABLE dbo.campus_snapshot (
